b4ce444f91b97b7b79f5ed469fda9b7f76d3ae24
miguel.eca.contractor <miguel.eca.contractor@bbva.com>
2022-11-17 00:40:53 -0500

    d1

--- assets/js/SlidePuzzle.js
+++ assets/js/SlidePuzzle.js
@@ -25,19 +25,31 @@
 //     }
 //   });
 // });
-
+[1, 14, 9, 3, 12, -1, 2, 6, 7, 10, 11, 4, 15, 5, 13, 8];
+const arr = [1, 14, 9, 3, 12, -1, 2, 6, 7, 10, 11, 4, 15, 5, 13, 8];
 $(function () {
   var squares = document.querySelectorAll(".square");
+
   squares.forEach((square) => {
     square.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log("target", e.target.dataset.pos);
-      var arr = []
-      arr.splice(1,1,)
+      // console.log(e.target.dataset.pos);
       var firstPos = parseInt(e.target.dataset.pos);
       var empty = document.querySelectorAll(".empty")[0];
       var secondPos = parseInt(empty.dataset.pos);
-      console.log("empty", empty.dataset.pos);
+      // console.log("empty", empty.dataset.pos);
+
+      var from = e.target.dataset.pos;
+      var to = empty.dataset.pos;
+      var value1 = arr[from];
+      var value2 = arr[to];
+
+      console.log("En la posicion", to, "esta", value1);
+      console.log("En la posicion", from, "esta", value2);
+
+      arr.splice(to, 1, value1);
+      arr.splice(from, 1, value2);
+      console.log(arr);
 
       var top = secondPos - 4;
       var bottom = secondPos + 4;
